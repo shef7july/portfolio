f5513801924c1721078e8f7388169f04f15a90b8
shef7july <shef.7july@gmail.com>
2023-04-10 14:46:36 -0700

    First Commit

---
+++ Contact.js
@@ -0,0 +1,14 @@
+// var MongoClient=require('mongodb').MongoClient;
+// var url="mongodb://localhost:27017/";
+
+// MongoClient.connect(url,function(err,db){
+//     if(err) throw err;
+//         var dbo=db.db("portfolio");
+//         var myobj={name:"Raj",mobile:6565656565,subject:"Cant Read",message:"I can read send me resume"};
+//         dbo.collection("portfolio_details").insertOne(myobj,functionerr,res){
+//             if(err) throw err;
+//             alert("Data successfully inserted");
+//             db.close();
+//         }
+
+// })
